b9cad27b9a613349c043edb55d2ebd912b38a349
ykylee <144748346+ykylee@users.noreply.github.com>
2025-06-17 16:27:47 +0900

    Translate SRS and update data features

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from .models import ExternalEmployee
 
 @admin.register(ExternalEmployee)
 class ExternalEmployeeAdmin(admin.ModelAdmin):
-    list_display = ("username", "full_name", "is_employed")
-    list_filter = ("is_employed",)
-    search_fields = ("username", "full_name")
+    list_display = ("username", "full_name", "employee_id", "department", "is_employed")
+    list_filter = ("is_employed", "department")
+    search_fields = ("username", "full_name", "employee_id", "department")
 
